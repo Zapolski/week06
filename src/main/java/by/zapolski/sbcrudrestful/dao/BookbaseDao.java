c212fd3d4eb9e0b7d4bfbe516ab0d5abac20f7dc
Siarhei_Zapolski <nestor.s@mail.ru>
2019-09-13 14:21:49 +0300

    develop

--- src/main/java/by/zapolski/sbcrudrestful/dao/BookbaseDao.java
+++
@@ -1,8 +0,0 @@
-package by.zapolski.sbcrudrestful.dao;
-
-import java.util.List;
-import java.util.Map;
-
-public interface BookbaseDao {
-	List<Map<String, Object>> queryForBooks(String partQuery); 
-}
